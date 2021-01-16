15a2bc04f24b8895d028e9fb9f1c3ad9ac37701a
Max Shipko <maximshipko@gmail.com>
2021-01-16 11:54:55 +0100

    feat: add selectors for genres, langs and countries; add validation and errors; add sumbit handler

--- src/components/MovieForm.tsx
+++ src/components/MovieForm.tsx
@@ -1,8 +1,14 @@
-import { Button, Grid } from "@material-ui/core";
+import {
+  Button,
+  Grid,
+  Divider,
+  InputAdornment,
+  CircularProgress,
+} from "@material-ui/core";
 
-import type { MovieDetailed, NewMovie } from "api/";
-import { useForm } from "common/";
-import { Form, Input } from "components/";
+import { MovieDetailed, NewMovie, genreApi, configApi, movieApi } from "api/";
+import { useFetcher, useForm, FormErrors } from "common/";
+import { Form, Input, Select } from "components/";
 
 type MovieFormProps = {
   children?: never;
@@ -18,110 +24,210 @@ const initialValues: NewMovie = {
   imdb_id: "",
   budget: 0,
   revenue: 0,
+  genre_ids: [],
+  original_language: "en",
+  spoken_languages_ids: [],
+  production_countries_ids: [],
+};
+const validator = (values: NewMovie) => {
+  const err: FormErrors<NewMovie> = {};
+  if ("title" in values && !values.title) {
+    err.title = "Title is required";
+  }
+  if ("release_date" in values && !values.release_date) {
+    err.release_date = "Release date is required";
+  }
+
+  if (
+    "imdb_id" in values &&
+    values.imdb_id &&
+    !/^tt[0-9]{7,8}/.test(values.imdb_id)
+  ) {
+    err.imdb_id = "IMDB ID should starts with 'tt' and includes 7—8 digits";
+  }
+  if ("genre_ids" in values && !values.genre_ids.length) {
+    err.genre_ids = "Select at lease one genre";
+  }
+
+  return err;
 };
 export const MovieForm = ({ movie }: MovieFormProps) => {
-  const { values, handleChange, handleSubmit } = useForm<NewMovie>({
-    initialValues: movie || initialValues,
-    onSubmit: (values) => console.log({ values }),
-  });
-
+  const { data: genresData } = useFetcher("Genres list", genreApi.list);
+  const { data: languages } = useFetcher(
+    "Language List",
+    configApi.languageList
+  );
+  const { data: countries } = useFetcher("Country List", configApi.countryList);
   const isUpdate = movie && movie.id;
+  const {
+    touched,
+    errors,
+    submitting,
+    getControlProps,
+    handleSubmit,
+  } = useForm<NewMovie>({
+    initialValues: movie || initialValues,
+    onSubmit: (values) => {
+      console.log("Submiting form", values);
+      if (isUpdate) {
+        return movieApi.update(values).catch((error) => {
+          alert(error); // TODO: make toast
+        });
+      } else {
+        return movieApi.create(values).catch((error) => {
+          alert(error); // TODO: make toast
+        });
+      }
+    },
+    validator: validator,
+  });
+  const handleDelete = () => {
+    if (!movie) return;
+    window.confirm("Are you sure to remove the movie?") &&
+      movieApi.remove(movie.id).catch((error) => {
+        alert(error); // TODO: make toast
+      });
+  };
+
   return (
     <Form onSubmit={handleSubmit} id="movieForm" noValidate>
       <Grid container>
         <Grid item xs={6}>
           <Input
-            name="title"
+            {...getControlProps("title")}
             label="Title"
-            value={values.title}
-            onChange={handleChange}
             placeholder="Title (EN)"
+            required
+            error={touched.title && Boolean(errors.title)}
+            helperText={touched.title && errors.title}
           />
           <Input
-            name="original_title"
+            {...getControlProps("original_title")}
             label="Original Title"
-            value={values.original_title}
-            onChange={handleChange}
           />
           <Input
-            name="release_date"
+            type="date"
+            {...getControlProps("release_date")}
             label="Release Date"
-            value={values.release_date}
-            onChange={handleChange}
+            required
+            InputLabelProps={{
+              shrink: true,
+            }}
+            error={touched.release_date && Boolean(errors.release_date)}
+            helperText={touched.release_date && errors.release_date}
           />
+          <Input {...getControlProps("poster_path")} label="Poster path" />
+          <Input {...getControlProps("backdrop_path")} label="Backdrop path" />
+          <Input {...getControlProps("homepage")} label="Homepage URL" />
           <Input
-            name="poster_path"
-            label="Poster path"
-            value={values.poster_path}
-            onChange={handleChange}
-          />
-          <Input
-            name="backdrop_path"
-            label="Backdrop path"
-            value={values.backdrop_path}
-            onChange={handleChange}
-          />
-          <Input
-            name="homepage"
-            label="Homepage URL"
-            value={values.homepage}
-            onChange={handleChange}
-          />
-          <Input
-            name="imdb_id"
             label="IMDB ID"
-            value={values.imdb_id}
-            onChange={handleChange}
-          />
-          <Input
-            name="budget"
-            type="number"
-            label="Budget"
-            value={values.budget}
-            onChange={handleChange}
+            {...getControlProps("imdb_id")}
             InputProps={{
-              inputProps: {
-                min: 0,
-              },
+              inputProps: { maxLength: 10 },
             }}
+            error={touched.imdb_id && Boolean(errors.imdb_id)}
+            helperText={touched.imdb_id && errors.imdb_id}
           />
-          <Input
-            name="revenue"
-            type="number"
-            label="Revenue"
-            value={values.revenue}
-            onChange={handleChange}
-            InputProps={{
-              inputProps: {
-                min: 0,
-              },
-            }}
-          />
-
-          <div>
-            Multi selector: genres, production_companies, production_countries,
-            spoken_languages
-          </div>
         </Grid>
         <Grid item xs={6}>
-          {" "}
+          <Input
+            {...getControlProps("budget")}
+            label="Budget"
+            type="number"
+            InputProps={{
+              inputProps: {
+                min: 0,
+              },
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              ),
+            }}
+          />
+          <Input
+            type="number"
+            label="Revenue"
+            {...getControlProps("revenue")}
+            InputProps={{
+              inputProps: {
+                min: 0,
+              },
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              ),
+            }}
+          />
+          {languages ? (
+            <Select
+              {...getControlProps("original_language")}
+              label="Original Language"
+              options={languages}
+            />
+          ) : null}
+          {genresData ? (
+            <Select
+              {...getControlProps("genre_ids")}
+              label="Genres"
+              options={genresData.genres}
+              multiple
+              required
+              error={touched.genre_ids && Boolean(errors.genre_ids)}
+              helperText={touched.genre_ids ? errors.genre_ids : ""}
+            />
+          ) : null}
+          {languages ? (
+            <Select
+              {...getControlProps("spoken_languages_ids")}
+              label="Spoken Languages"
+              options={languages}
+              multiple
+            />
+          ) : null}
+          {countries ? (
+            <Select
+              {...getControlProps("production_countries_ids")}
+              label="Production Countries"
+              options={countries}
+              multiple
+            />
+          ) : null}
         </Grid>
         <Grid item xs={12}>
+          <Divider />
+          <br />
           <div>
             {isUpdate ? (
-              <Button variant="outlined" color="secondary">
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={handleDelete}
+              >
                 Remove
               </Button>
             ) : null}
             {isUpdate ? (
-              <Button variant="contained" color="primary" type="submit">
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={submitting}
+              >
                 Update
               </Button>
             ) : (
-              <Button variant="contained" color="primary" type="submit">
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={submitting}
+              >
                 Create
               </Button>
-            )}
+            )}{" "}
+            {submitting ? (
+              <>
+                <CircularProgress size={16} /> Submiting ...
+              </>
+            ) : null}
           </div>
         </Grid>
       </Grid>
