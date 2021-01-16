13edb360fb89df6cb5b32ff44db689de51e8e873
Max Shipko <maximshipko@gmail.com>
2021-01-16 11:46:51 +0100

    feat: rework useForm hook

--- src/common/useForm.ts
+++ src/common/useForm.ts
@@ -1,59 +1,99 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
-type FixMe = unknown;
-export const useForm = <T extends object>({
+export interface FormValues {
+  [field: string]: any;
+}
+
+export type FormErrors<V> = {
+  [key in keyof V]?: string;
+};
+export type FormTouched<V> = {
+  [key in keyof V]?: boolean;
+};
+type UseFormProps<V> = {
+  initialValues: V;
+  onSubmit: (values: V) => Promise<unknown>;
+  validator: (values: V) => FormErrors<V>;
+};
+export const useForm = <V extends FormValues>({
   initialValues,
   onSubmit,
-}: {
-  initialValues: T;
-  onSubmit: (opt: FixMe) => void;
-}) => {
-  const [values, setValues] = useState<T>(initialValues);
-  const [errors, setErrors] = useState<object>({});
-  const [touched, setTouched] = useState<object>({});
+  validator,
+}: UseFormProps<V>) => {
+  const [values, setValues] = useState<V>(initialValues);
+  const [errors, setErrors] = useState<FormErrors<V>>({});
+  const [touched, setTouched] = useState<FormTouched<V>>({});
   const [submitting, setSubmitting] = useState<boolean>(false);
-  // const [onBlur, setOnBlur] = useState<boolean>(false);
-
-  const formRendered = useRef(true);
 
   useEffect(() => {
-    if (formRendered.current) {
-      setValues(initialValues);
-      setErrors({});
-      setTouched({});
-      setSubmitting(false);
-      // setOnBlur(false);
+    if (validator) {
+      setErrors(validator(values));
     }
-    formRendered.current = false;
-  }, [initialValues]);
+  }, [values, validator]);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { target } = event;
-    const { name, value = "" } = target;
-    setValues({ ...values, [name]: value });
+  const handleChange = (field: keyof V) => (
+    event: React.ChangeEvent<
+      HTMLTextAreaElement | HTMLInputElement | { value: unknown }
+    >
+  ) => {
+    setValues({ ...values, [field]: event.target.value });
   };
 
-  const handleBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { target } = event;
-    const { name } = target;
-    setTouched({ ...touched, [name]: true });
-    setErrors({ ...errors });
+  const handleBlur = (field: keyof V) => (
+    event: React.FocusEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setTouched({ ...touched, [field]: true });
+  };
+
+  const resetForm = () => {
+    setValues(initialValues);
+    setErrors({});
+    setTouched({});
   };
 
   const handleSubmit = (event: any) => {
     if (event) event.preventDefault();
     if (submitting) return;
-    setErrors({ ...errors });
-    setSubmitting(true);
-    onSubmit({ values, errors });
+    const err = validator(values);
+    if (Object.keys(err).length) {
+      setErrors(err);
+      setTouched(
+        Object.keys(values).reduce(
+          (acc: FormTouched<V>, key: keyof FormTouched<V>) => {
+            acc[key] = true;
+            return acc;
+          },
+          {}
+        )
+      );
+      setSubmitting(false);
+    } else {
+      setSubmitting(true);
+
+      return onSubmit(values).finally(() => {
+        setSubmitting(false);
+      });
+    }
+  };
+
+  const getControlProps = (name: keyof V) => {
+    return {
+      name,
+      value: values[name] || "",
+      onChange: handleChange(name),
+      onBlur: handleBlur(name),
+    };
   };
 
   return {
     values,
     errors,
     touched,
+    submitting,
     handleChange,
     handleBlur,
+    getControlProps,
+    resetForm,
     handleSubmit,
   };
 };
